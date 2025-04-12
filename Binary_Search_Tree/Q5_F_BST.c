ef2efce3dd49815685358c7a2a3ac2824277717f
StrongDeutan <dsb000918@gmail.com>
2025-04-13 00:38:18 +0900

    last

--- Binary_Search_Tree/Q5_F_BST.c
+++ Binary_Search_Tree/Q5_F_BST.c
@@ -92,7 +92,27 @@ int main()
 void postOrderIterativeS2(BSTNode *root)
 {
 	 /* add your code here */
-	 
+	 if(root == NULL) return;
+	 Stack s1;
+	 Stack s2;
+	 s1.top = NULL;
+	 s2.top = NULL;
+
+	push(&s1, root);
+	while(!isEmpty(&s1)){
+		BSTNode* cur = pop(&s1);
+		push(&s2, cur);
+		if(cur->left != NULL){
+			push(&s1, cur->left);
+		}
+		if(cur->right != NULL){
+			push(&s1, cur->right);
+		}
+	}
+	while(!isEmpty(&s2)){
+		BSTNode* cur = pop(&s2);
+		printf("%i ", cur->item);
+	}
 }
 
 /* Given a binary search tree and a key, this function
